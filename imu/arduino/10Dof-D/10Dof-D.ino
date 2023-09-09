610ea625f259de54eb46ebd940fda6d719345a33
Victor Kats <victor.kats@compvisionsys.com>
2023-09-09 15:47:49 +0300

    Waveshare 10 DOF IMU Sensor acceptance code initial commit

---
+++ imu/arduino/10Dof-D/10Dof-D.ino
@@ -0,0 +1,29 @@
+#include "Waveshare_10Dof-D.h"
+
+void setup() {
+  IMU_EN_SENSOR_TYPE enMotionSensorType, enPressureType;
+  Serial.begin(115200);
+
+  imuInit(&enMotionSensorType, &enPressureType);
+  if(IMU_EN_SENSOR_TYPE_ICM20948 == enMotionSensorType)
+  {
+    Serial.println("Motion sersor is ICM-20948");
+  }
+  else
+  {
+    Serial.println("Motion sersor NULL");
+  }
+  if(IMU_EN_SENSOR_TYPE_BMP280 == enPressureType)
+  {
+    Serial.println("Pressure sersor is BMP280");
+  }
+  else
+  {
+    Serial.println("Pressure sersor NULL");
+  }
+  delay(1000);
+}
+
+void loop() {
+
+}
